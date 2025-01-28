c7384201f1e74bca896c0aca26f1ad5d9c1afdbe
Leonardo <LeonardoChe888@ya.ru>
2025-01-28 18:31:36 +0700

    изменение способа хранения данных

--- game_screen.py
+++ game_screen.py
@@ -1,5 +1,3 @@
-from math import gamma
-
 import pygame
 from pygame import Color
 
@@ -17,7 +15,15 @@ class Board:
     def __init__(self, n=3):
         self.n = n  # количество клеток в районе
         self.board = task
-        self.pressed_cell = [[False] * (n ** 2) for _ in range(n ** 2)]
+        for y in range(n ** 2):
+            for x in range(n ** 2):
+                val = self.board[y][x]
+                if val:
+                    self.board[y][x] = {"num": val, "change": False, "selected": False}
+                else:
+                    self.board[y][x] = {"num": 0, "change": True, "selected": False}
+
+        # self.pressed_cell = [[False] * (n ** 2) for _ in range(n ** 2)]
         self.left = 10
         self.top = 10
         self.cell_size = 40
@@ -27,8 +33,8 @@ class Board:
         self.moment_choice = False
         self.missing_nums = missing_nums
         self.last_selected_cell = None
-        self.keys_dict = list(self.missing_nums.keys())
-        self.game_board = [list(map(lambda x: False if x > 0 else 0, i)) for i in task]
+        self.keys_missing_nums = list(self.missing_nums.keys())
+        # self.game_board = [list(map(lambda x: False if x > 0 else 0, i)) for i in task]
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -50,38 +56,35 @@ class Board:
 
     def press_cell(self, pos):
         if pos[2] == "big":
-            if not self.board[pos[1]][pos[0]]:
+            if not self.board[pos[1]][pos[0]]["num"]:
                 if self.last_selected_cell:
-                    self.pressed_cell[self.last_selected_cell[1]][self.last_selected_cell[0]] = False
-                self.pressed_cell[pos[1]][pos[0]] = True
+                    self.board[self.last_selected_cell[1]][self.last_selected_cell[0]]["selected"] = False
+                self.board[pos[1]][pos[0]]["selected"] = True
                 self.moment_choice = True
                 self.last_selected_cell = pos
-                self.moment_choice = True
             else:
                 self.moment_choice = False
         if pos[2] == "small":
             if self.moment_choice:
-                num = self.keys_dict[pos[0]]
+                num = self.keys_missing_nums[pos[0]]
                 if self.missing_nums[num] != 0:
-                    self.board[self.last_selected_cell[1]][self.last_selected_cell[0]] = num
-                    self.game_board[self.last_selected_cell[1]][self.last_selected_cell[0]] = num
+                    self.board[self.last_selected_cell[1]][self.last_selected_cell[0]]["num"] = num
                     self.missing_nums[num] -= 1
 
     def press_back(self, pos):
         x, y, _ = pos
         if pos[2] == "big":
-            if self.game_board[y][x]:
-                num = self.board[y][x]
-                self.board[y][x] = 0
+            if self.board[y][x]["change"] and self.board[y][x]["num"]:
+                num = self.board[y][x]["num"]
+                self.board[y][x]["num"] = 0
                 self.missing_nums[num] += 1
-                self.game_board[y][x] = 0
-                self.pressed_cell[y][x] = False
+                self.board[y][x]["selected"] = False
 
     def render(self, screen):
         x, y = self.left, self.top
         for h in range(self.n ** 2):
             for w in range(self.n ** 2):
-                if self.pressed_cell[h][w]:
+                if self.board[h][w]["selected"]:
                     pygame.draw.rect(screen, pygame.Color("red"), (x, y, self.cell_size, self.cell_size), 6)
                 if h > 0 and h % 3 == 0:
                     pygame.draw.line(screen, pygame.Color("white"), (self.left, y),
@@ -91,11 +94,15 @@ class Board:
                     pygame.draw.line(screen, pygame.Color("white"), (x, self.top),
                                      (x, self.top + self.cell_size * 9 - 1), self.line_width)
                     # вертикальные
-                if self.board[h][w] == 0:
+                if self.board[h][w]["num"] == 0:
                     pygame.draw.rect(screen, pygame.Color("white"), (x, y, self.cell_size, self.cell_size), 1)
                 else:
                     font = pygame.font.Font(None, 35)
-                    text = font.render(str(self.board[h][w]), True, (100, 255, 100))
+                    if self.board[h][w]["change"]:
+                        text = font.render(str(self.board[h][w]["num"]), True, pygame.Color("blue"))
+                    else:
+                        text = font.render(str(self.board[h][w]["num"]), True, (100, 255, 100))
+
                     screen.blit(text, (x + 12, y + 8))
                     pygame.draw.rect(screen, pygame.Color("white"), (x, y, self.cell_size, self.cell_size), 1)
                 x += self.cell_size
@@ -108,12 +115,12 @@ class Board:
             for w in range(self.n ** 2):
                 if h == 0:
                     font = pygame.font.Font(None, 35)
-                    text = font.render(str(self.keys_dict[w]), True, Color("blue"))
+                    text = font.render(str(self.keys_missing_nums[w]), True, Color("blue"))
                     screen.blit(text, (x + 12, y + 8))
                     pygame.draw.rect(screen, pygame.Color("white"), (x, y, self.cell_size, self.cell_size), 1)
                 else:
                     font = pygame.font.Font(None, 35)
-                    text = font.render(str(self.missing_nums[self.keys_dict[w]]), True, Color("blue"))
+                    text = font.render(str(self.missing_nums[self.keys_missing_nums[w]]), True, Color("blue"))
                     screen.blit(text, (x + 12, y + 8))
                     pygame.draw.rect(screen, pygame.Color("white"), (x, y, self.cell_size, self.cell_size), 1)
 

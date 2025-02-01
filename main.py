ee0ee4afd3dd67c87494dbe1dbafedf6668d0403
Leonardo <LeonardoChe888@ya.ru>
2025-02-01 23:29:58 +0700

    отрисовка меню

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from game_screen import Board
+from pygame.examples.moveit import WIDTH
 
+from game_screen import Board
+from start_screen import StartScreen
 window = ["start_screen", "game_screen"]
 window_pos = 1
 
@@ -8,14 +10,21 @@ window_pos = 1
 def main():
     fps = 60
     pygame.init()
+    width = 501
+    height = 501
     screen = pygame.display.set_mode((501, 501))
     board = Board()
+    start_screen = StartScreen(width, height)
     clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if window[window_pos] == "start_screen":
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    start_screen.click(event.pos)
+
 
             if window[window_pos] == "game_screen":
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -33,6 +42,9 @@ def main():
 
         if window[window_pos] == "game_screen":
             board.render(screen)
+
+        if window[window_pos] == "start_screen":
+            start_screen.render(screen)
         pygame.display.update()
         screen.fill((0,0,0))
         clock.tick(fps)
--- start_screen.py
+++ start_screen.py
@@ -1,4 +1,71 @@
+import pygame
+
+
 def start_screen():
-     # реализовать авторизацию, рейтинг
+    # реализовать авторизацию, рейтинг
     pass
 
+
+class StartScreen:
+    def __init__(self, width, height):
+        self.__window = ["menu", "sign in", "rating"]
+        self.__window_pos = 0
+        self.__width = width
+        self.__height = height
+        self.__label = None
+        self.__sign_in = None
+        self.__rating = None
+        self.__input_user = None
+        self.__input_moment = -1
+
+    def render(self, screen):
+        color = "red"
+
+        if self.__window[self.__window_pos] == "menu":
+            top = 0
+            step = 20
+            for i in ["Sudoku", "Sign in", "Rating"]:
+                font = pygame.font.Font(None, 40)
+                if i == "Sudoku":
+                    font = pygame.font.Font(None, 70)
+                str_render = font.render(i, 1, pygame.Color(color))
+                intro_rect = str_render.get_rect()
+                if i == "Sudoku":
+                    intro_rect = [self.__width // 2 - intro_rect[2] // 2,
+                                  (self.__height // 2 - intro_rect[3] // 2 - 50), intro_rect[2], intro_rect[3]]
+                else:
+                    intro_rect = [self.__width // 2 - intro_rect[2] // 2, top, intro_rect[2], intro_rect[3]]
+                screen.blit(str_render, intro_rect)
+                if i == "Sudoku":
+                    self.__label = pygame.Rect(intro_rect)
+                    top = self.__label.y + self.__label.height
+                if i == "Sign in":
+                    self.__sign_in = pygame.Rect(intro_rect)
+                    top = self.__sign_in.y + self.__sign_in.height
+                if i == "Rating":
+                    self.__rating = pygame.Rect(intro_rect)
+                    top = self.__rating.y + self.__rating.height
+                top += step
+
+        if self.__window[self.__window_pos] == "sign in":
+            name = "name"
+            font = pygame.font.Font(None, 70)
+            str_render = font.render(name, 1, pygame.Color(color))
+            intro_rect = str_render.get_rect()
+            intro_rect = [self.__width // 2 - intro_rect[2] // 2, (self.__height // 2 - intro_rect[3] // 2 - 50),
+                          intro_rect[2], intro_rect[3]]
+            screen.blit(str_render, intro_rect)
+            intro_rect[2] += 100
+            intro_rect = [self.__width // 2 - intro_rect[2] // 2, (self.__height // 2 - intro_rect[3] // 2 - 50),
+                          intro_rect[2], intro_rect[3]]
+            self.__input_user = pygame.Rect(intro_rect)
+            pygame.draw.rect(screen, pygame.Color(color), intro_rect, 1)
+
+    def click(self, pos):
+        if self.__window[self.__window_pos] == "menu":
+            if self.__sign_in and self.__sign_in.collidepoint(pos):
+                self.__window_pos = 1
+
+        if self.__window[self.__window_pos] == "sign in":
+            if self.__input_user and self.__input_user.collidepoint(pos):
+                self.__input_moment *= -1
